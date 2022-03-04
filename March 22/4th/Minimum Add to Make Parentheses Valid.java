9ee4649139cdace80b87865667bd39cfefd53a9b
Ankit <88306222+ank1tt@users.noreply.github.com>
2022-03-04 07:19:47 +0530

    Create Minimum Add to Make Parentheses Valid.java

---
+++ March 22/4th/Minimum Add to Make Parentheses Valid.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/minimum-add-to-make-parentheses-valid
+
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> ans = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            // if the stack is empty we just push the element.
+            if(ans.isEmpty()){
+                ans.push(ch);
+                // if the stack is balanced we remove the whole balanced string.
+            }else if(ans.peek() == '(' && ch == ')'){
+                ans.pop();
+            }else{
+                //if not balanced just push the character
+                ans.push(ch);
+            }
+        }
+        // the no required to balance the string is basically equal to the size of the stack.
+        return ans.size();
+    }
+}
